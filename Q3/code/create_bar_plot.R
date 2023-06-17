f6ab7074d9718e00f3d2d37742ee9aa3844b9214
hanmacg <21082022@sun.ac.za>
2023-06-17 16:23:35 +0200

    fixing up Q3

---
+++ Q3/code/create_bar_plot.R
@@ -0,0 +1,11 @@
+create_bar_plot <- function(data, x_col, y_col, xlab_text, ylab_text, title_text) {
+    bar_plot <- data %>%
+        ggplot(aes({{ x_col }}, {{ y_col }})) +
+        geom_bar(stat = "identity", fill = "steelblue") +
+        xlab({{ xlab_text }}) +
+        ylab({{ ylab_text }}) +
+        ggtitle({{ title_text }})
+
+    return(bar_plot)
+}
+
